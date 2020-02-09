761ddee3b807b908a24a6a1c185b58d3f80d847c
Tlangelani Mboweni <mbowenit@gmail.com>
2020-02-09 12:34:15 +0200

    update vue and add factory data

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,6 +7,7 @@
 require('./bootstrap');
 
 window.Vue = require('vue');
+Vue.use(require('vue-resource'));
 
 /**
  * The following block of code may be used to automatically register your
@@ -20,6 +21,7 @@ window.Vue = require('vue');
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('load-more-component', require('./components/LoadMoreComponent.vue'));
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
